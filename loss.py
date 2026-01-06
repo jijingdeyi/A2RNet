02e5fc1ae6f08b33f6f79ac82b5d5eaf1a336c5c
Kaixuan Yang <jijingdeyi@gmail.com>
2026-01-06 11:30:06 +0800

    写出了一版融合损失

--- loss.py
+++ loss.py
@@ -4,21 +4,6 @@ import torch.nn.functional as F
 from pytorch_msssim import ssim
 
 
-class SoftmaxFocalLoss(nn.Module):
-    def __init__(self, gamma, ignore_lb=255, *args, **kwargs):
-        super(SoftmaxFocalLoss, self).__init__()
-        self.gamma = gamma
-        self.nll = nn.NLLLoss(ignore_index=ignore_lb)
-
-    def forward(self, logits, labels):
-        scores = F.softmax(logits, dim=1)
-        factor = torch.pow(1.-scores, self.gamma)
-        log_score = F.log_softmax(logits, dim=1)
-        log_score = factor * log_score
-        loss = self.nll(log_score, labels)
-        return loss
-
-
 class Sobelxy(nn.Module):
     def __init__(self):
         super(Sobelxy, self).__init__()
@@ -30,32 +15,48 @@ class Sobelxy(nn.Module):
                    [-1, -2, -1]]
         kernelx = torch.FloatTensor(kernelx).unsqueeze(0).unsqueeze(0)
         kernely = torch.FloatTensor(kernely).unsqueeze(0).unsqueeze(0)
-        self.weightx = nn.Parameter(data=kernelx, requires_grad=False).cuda()
-        self.weighty = nn.Parameter(data=kernely, requires_grad=False).cuda()
+        self.weightx = nn.Parameter(data=kernelx, requires_grad=False)
+        self.weighty = nn.Parameter(data=kernely, requires_grad=False)
     def forward(self,x):
         sobelx = F.conv2d(x, self.weightx, padding=1)
         sobely = F.conv2d(x, self.weighty, padding=1)
         return torch.abs(sobelx)+torch.abs(sobely)
 
-    
-class fusion_loss_adv(nn.Module):
+
+class fusion_loss(nn.Module):
     def __init__(self):
-        super(fusion_loss_adv, self).__init__()
+        super(fusion_loss, self).__init__()
         self.sobelconv = Sobelxy()
 
-    def forward(self, generate_img_adv, generate_img):
-        # L_mse
-        loss_mse = F.mse_loss(generate_img_adv, generate_img)
+    def forward(self, ir, vis, fused):
 
-        # L_ssim
-        loss_ssim = 1 - ssim(generate_img_adv, generate_img)
+        y_grad = self.sobelconv(vis)
+        fused_grad = self.sobelconv(fused)
+        loss_grad = F.l1_loss(y_grad, fused_grad)
 
-        # L_total
-        loss_total = 100 * loss_mse + 100 * loss_ssim
-        return loss_total, loss_mse, loss_ssim
+        loss_ssim = 1 - ssim(fused, vis, data_range=1.0, size_average=True)
+
+        k = 2 #temperature parameter
+        ir_mean = ir.mean(dim=[2,3], keepdim=True)
+        omega_ir = torch.sigmoid(k * (ir - ir_mean)).detach()
+        loss_sal = (omega_ir * (fused - ir).abs()).mean()
+
+        vis_mean  = vis.mean(dim=[2,3], keepdim=True)
+        fused_mean = fused.mean(dim=[2,3], keepdim=True)
+        loss_mean = (fused_mean - vis_mean).abs().mean()
 
 
+        tv_h = torch.abs(fused[:, :, 1:, :] - fused[:, :, :-1, :]).mean()
+        tv_w = torch.abs(fused[:, :, :, 1:] - fused[:, :, :, :-1]).mean()
+        loss_tv = tv_h + tv_w
 
-if __name__ == '__main__':
-    pass
+
+        alpha = 1
+        beta = 5
+        gamma = 1
+        rho = 0.2
+        delta = 0.001
+        loss_fusion = alpha * loss_sal + beta * loss_grad + gamma * loss_ssim + rho * loss_mean + delta * loss_tv
+        return loss_fusion
+
 
